519402bef8b35d73c8d34278f477cae84177517b
Sean OReilly <soreilly424@gmail.com>
2021-01-26 09:42:44 -0500

    learning async await

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import './App.css';
-import Searchbar from './Searchbar'
-
+import React from "react";
+import axios from 'axios'
+import "./App.css";
+import Searchbar from "./Searchbar";
 
 class App extends React.Component {
-
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID OOXgpJep-U0YYehyWBQVK2IWdauRILQQp8vAIhunGnM"
+      },
+    })
+    console.log(response.data.results);
   }
 
   render() {
     return (
-      <div className="ui container" style={{marginTop: '10px'}}>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <Searchbar onSubmit={this.onSearchSubmit} />
       </div>
     );
